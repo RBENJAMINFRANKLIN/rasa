908484f90f3e596bab1a4d68ecfd8f3a86a306e6
alwx <alwxndr@gmail.com>
2020-07-28 14:56:48 +0200

    Excluding Win32-specific tests

--- rasa/core/channels/slack.py
+++ rasa/core/channels/slack.py
@@ -48,7 +48,7 @@ class SlackBot(OutputChannel):
         recipient = self.slack_channel or recipient_id
         for message_part in text.strip().split("\n\n"):
             await self._post_message(
-                channel=recipient, as_user=True, text=message_part, type="mrkdwn",
+                channel=recipient, as_user=True, text=message_part, type="mrkdwn"
             )
 
     async def send_image_url(
@@ -58,7 +58,7 @@ class SlackBot(OutputChannel):
         image_block = {"type": "image", "image_url": image, "alt_text": image}
 
         await self._post_message(
-            channel=recipient, as_user=True, text=image, blocks=[image_block],
+            channel=recipient, as_user=True, text=image, blocks=[image_block]
         )
 
     async def send_attachment(
@@ -66,7 +66,7 @@ class SlackBot(OutputChannel):
     ) -> None:
         recipient = self.slack_channel or recipient_id
         await self._post_message(
-            channel=recipient, as_user=True, attachments=[attachment], **kwargs,
+            channel=recipient, as_user=True, attachments=[attachment], **kwargs
         )
 
     async def send_text_with_buttons(
